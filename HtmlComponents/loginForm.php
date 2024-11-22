2a5f65d50d9ea35994fc2c9c997727849510d579
MilosJovic23 <milos.mj992@gmail.com>
2024-11-22 12:23:45 +0100

    * working on user login and register logic in class User

---
+++ HtmlComponents/loginForm.php
@@ -0,0 +1,21 @@
+
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
+    </head>
+    <body>
+        <div>
+            <form method="POST" action="../models/login.php">
+                <input type="email" placeholder="email" name="email" >
+                <input type="password" placeholder="password" name="password">
+                <button type="submit">login</button>
+            </form>
+        </div>
+    </body>
+</html>
