34f6992d3b575f356208492eefd5a08e4d19c1e6
MilosJovic23 <milos.mj992@gmail.com>
2024-11-26 10:11:19 +0100

    * added register logic

--- HtmlComponents/navbar.php
+++ HtmlComponents/navbar.php
@@ -22,7 +22,7 @@
         <nav>
             <ul>
                 <a href="/Epictown"><li>home</li></a>
-                <?php if( isset($_SESSION['loggedIn'])): ?>
+                <?php if( isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']): ?>
                 <a href="/Epictown/models/logout.php"><li>Logout</li></a>
                 <?php else: ?>
                 <a href="/Epictown/HtmlComponents/loginForm.php"><li>login</li></a>
