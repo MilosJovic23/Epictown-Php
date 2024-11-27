098163352240242b4fdf482afc1835c9855c7692
MilosJovic23 <milos.mj992@gmail.com>
2024-11-27 12:42:42 +0100

    * small changes

--- HtmlComponents/navbar.php
+++ HtmlComponents/navbar.php
@@ -1,24 +1,4 @@
 
-<?php
-
-
-    if ( session_status() == PHP_SESSION_NONE ) {
-        session_start();
-    }
-
-
-?>
-
-
-
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Document</title>
-    </head>
-    <body>
         <nav>
             <ul>
                 <a href="/Epictown"><li>home</li></a>
@@ -29,5 +9,3 @@
                 <?php endif; ?>
             </ul>
         </nav>
-    </body>
-</html>
