e1f29dd4b74f496243dd15fc80d072e6f8a8fefe
MilosJovic23 <milos.mj992@gmail.com>
2024-11-28 10:21:58 +0100

    * small changes

--- HtmlComponents/navbar.php
+++ HtmlComponents/navbar.php
@@ -1,11 +1,12 @@
 
         <nav>
             <ul>
-                <a href="/Epictown"><li>home</li></a>
+                    <a href="/Epictown"><li>Home</li></a>
                 <?php if( isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']): ?>
-                <a href="/Epictown/models/logout.php"><li>Logout</li></a>
+                    <a href="/Epictown/HtmlComponents/dashboard.php"><li>Dashboard</li></a>
+                    <a href="/Epictown/models/logout.php"><li>logout</li></a>
                 <?php else: ?>
-                <a href="/Epictown/HtmlComponents/loginForm.php"><li>login</li></a>
+                    <a href="/Epictown/HtmlComponents/loginForm.php"><li>login</li></a>
                 <?php endif; ?>
             </ul>
         </nav>
