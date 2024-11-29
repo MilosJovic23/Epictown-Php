3d145a5e728a8d192917b16b13fa0550f7be57c2
MilosJovic23 <milos.mj992@gmail.com>
2024-11-29 11:52:38 +0100

    * small changes

---
+++ HtmlComponents/search.php
@@ -0,0 +1,9 @@
+
+
+
+    <div>
+        <form method="GET" action="/Epictown/models/search.php">
+            <input name="search" placeholder="search comics"/>
+            <button>search</button>
+        </form>
+    </div>
