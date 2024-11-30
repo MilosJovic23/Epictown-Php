3ed6cf7293620968b6da25cb995a6259afee26d6
MilosJovic23 <milos.mj992@gmail.com>
2024-11-30 17:05:23 +0100

    * added list of comics to dashbaord page

--- HtmlComponents/dashboard.php
+++ HtmlComponents/dashboard.php
@@ -3,8 +3,17 @@
 <?php
 
 
+    if( session_status() == PHP_SESSION_NONE ){
+        session_start();
+    }
 
+    require_once "../classes/Database.php";
 
+    $db = new Database();
+    $conn = $db->connection;
+
+    $result = $conn->query("SELECT * FROM comicbooks");
+    $allComics = $result->fetch_all(MYSQLI_ASSOC);
 
 
 ?>
@@ -18,14 +27,32 @@
         <title>Dashboard</title>
     </head>
     <body>
-        <form method="GET" action="../models/addNewComic.php">
-            <input type="text" placeholder="description" name="description">
-            <input type="text" placeholder="title" name="title">
-            <input type="text" placeholder="format" name="format">
-            <input type="text" placeholder="imgURL" name="imgURL">
-            <input type="text" placeholder="author" name="author">
-            <input type="number" placeholder="rating" name="rating" step="0.1" min="0" max="5" >
-            <button type="submit">add</button>
-        </form>
+        <?php require_once "navbar.php"; ?>
+        <div>
+            <h3>add new comicbook</h3>
+            <form method="GET" action="../models/addNewComic.php">
+                <input type="text" placeholder="description" name="description">
+                <input type="text" placeholder="title" name="title">
+                <input type="text" placeholder="format" name="format">
+                <input type="text" placeholder="imgURL" name="imgURL">
+                <input type="text" placeholder="author" name="author">
+                <input type="number" placeholder="rating" name="rating" step="0.1" min="0" max="5" >
+                <button type="submit">add</button>
+            </form>
+        </div>
+        <?php foreach( $allComics as $comic ): ?>
+            <div style="width: 150px;">
+                <a href="singleProduct.php?id=<?= $comic['id'] ?>">
+                    <img src="<?= $comic['imgURL'] ?>" style="width: 150px;">
+                </a>
+                <h4> <?=  $comic['title'] ?></h4>
+                <i> author: <?=  $comic['author'] ?></i>
+                <p> rating: <?=  $comic['rating'] ?></p>
+                <form method="GET" action="../models/deleteComic.php">
+                    <input name="id" value="<?= $comic['id'] ?>" type="hidden">
+                    <button type="submit">delete</button>
+                </form>
+            </div>
+        <?php endforeach; ?>
     </body>
 </html>
