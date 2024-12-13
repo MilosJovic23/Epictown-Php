88ed0b40a293b6c5dfde46a21fcc2b872a7eb34c
MilosJovic23 <milos.mj992@gmail.com>
2024-12-13 16:56:22 +0100

    * creating search api endpoint

--- restapi/search/api.php
+++ restapi/search/api.php
@@ -3,11 +3,19 @@
 
 
     $method = $_SERVER["REQUEST_METHOD"];
-    $input = json_decode(file_get_contents('php://input'));
+    $input = json_decode(file_get_contents('php://input'),true);
 
     require_once "../../classes/ComicBooks.php";
 
-    $comicbook = new ComicBooks();
-    $comicbook->search($input);
+    $searchTerm = $input["search"];
 
 
+    switch ($method) {
+        case 'POST':
+
+            $comicbook = new ComicBooks();
+            $comicbook->search($searchTerm);
+
+            echo json_encode($comicbook);
+            break;
+    }
