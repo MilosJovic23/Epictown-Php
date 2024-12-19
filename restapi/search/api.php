c4475fbb69658bc70ba1a507fb0d415d455a113c
MilosJovic23 <milos.mj992@gmail.com>
2024-12-19 11:17:47 +0100

    * few changes

--- restapi/search/api.php
+++ restapi/search/api.php
@@ -10,15 +10,15 @@
     switch ($method) {
         case 'POST':
             $searchTerm = $input["search"];
-            $comicbook = new ComicBooks();
-            $result = $comicbook->search($searchTerm);
+            $comics = new ComicBooks();
+            $result = $comics->search($searchTerm);
             echo is_array($result) ? json_encode($result) : json_encode(["message" => "No results found"]);
             http_response_code(201);
             break;
         default:
-            http_response_code(405);
             header('Allow: POST');
             echo json_encode(["error" => "Method not allowed"]);
+            http_response_code(405);
             break;
 
     }
