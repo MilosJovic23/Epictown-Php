a91b106cdab7f22c6fabeffc2d411c94106f98b1
MilosJovic23 <milos.mj992@gmail.com>
2025-01-15 17:50:46 +0100

    * few changes in header

--- restapi/api.php
+++ restapi/api.php
@@ -4,7 +4,7 @@
 
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
 
     include ("../classes/ComicBooks.php");
 
