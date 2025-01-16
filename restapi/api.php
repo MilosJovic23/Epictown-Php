be04c7b86aa6e13176592110b08f6262bfdd2e3c
MilosJovic23 <milos.mj992@gmail.com>
2025-01-16 14:18:23 +0100

    * few changes in header

--- restapi/api.php
+++ restapi/api.php
@@ -5,7 +5,7 @@
 
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-    header("Access-Control-Allow-Origin: http://localhost:3001");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
