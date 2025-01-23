303ad395e691506f313260f970ec4a9cd78449a1
MilosJovic23 <milos.mj992@gmail.com>
2025-01-23 11:49:07 +0100

    * setting up api for user login

--- restapi/login/api.php
+++ restapi/login/api.php
@@ -2,15 +2,26 @@
 
 
     header("Content-type: application/json");
+    header("Access-Control-Allow-Origin: POST");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+
     include ("../../classes/User.php");
 
     $user = new User();
     $conn = $user->connection;
-
     $method = $_SERVER["REQUEST_METHOD"];
-    $input = json_decode(file_get_contents('php://input'));
-    $userExist = $user->register($input['username'], $input['password']);
+    $input = json_decode(file_get_contents('php://input'),true);
 
-    if($userExist){
-        echo json_encode(["message" => "User already exists"]);
+    if ( $method !== "POST" ) {
+        echo json_encode(["message" => "Method not allowed"]);
+        exit;
     }
+    $result = $user->login($input);
+    if( !$result ){
+        echo json_encode(["message" => "wrong credentials"]);
+        http_response_code(401);
+        exit;
+    }
+    http_response_code(202);
+    echo json_encode(["message"=>"success"]);
+
