cf4949db9ee6cf801e51d6cb91bab282e7c69db3
MilosJovic23 <milos.mj992@gmail.com>
2025-01-23 11:49:43 +0100

    * setting up class User for  login and registration

--- classes/User.php
+++ classes/User.php
@@ -3,18 +3,18 @@
     require_once "Database.php";
     class User extends Database {
 
-        public function login($email, $password) :bool {
+        public function login($input) :bool {
 
-            $email = mysqli_real_escape_string( $this->connection,$email );
+            $email = mysqli_real_escape_string( $this->connection,$input["username"] );
+            $password = mysqli_real_escape_string( $this->connection,$input["password"] );
             $result = $this->connection->query("SELECT * FROM users WHERE email = '$email'");
 
-            if ( $result->num_rows == 0 ) {
+            if ( $result->num_rows === 0 ) {
                 die("user not found with this email");
             }
 
             $user = $result->fetch_assoc();
             $loggedIn = false;
-
             if ( password_verify( $password, $user['password'] ) ) {
                 $loggedIn = true;
             }
