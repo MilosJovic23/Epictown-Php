bcfb9ca423bb34342595badef3b4ea708f7b2f83
MilosJovic23 <milos.mj992@gmail.com>
2025-01-29 13:00:38 +0100

    * deleted unused files

--- HtmlComponents/search.php
+++
@@ -1,9 +0,0 @@
-
-
-
-    <div>
-        <form method="GET" action="/Epictown/models/search.php">
-            <input name="search" placeholder="search comics"/>
-            <button>search</button>
-        </form>
-    </div>
